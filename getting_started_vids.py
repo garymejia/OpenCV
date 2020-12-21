b2404ed9aeaf1577baa0d0708b794e8d02713ac9
garymejia <fjgmejia@gmail.com>
2020-12-21 16:36:04 -0500

    getting started with saving videos

--- getting_started_vids.py
+++ getting_started_vids.py
@@ -3,25 +3,40 @@ import cv2 as cv
 
 #argument can be either the device index or the name of the video file
 cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("cannot open camera")
-    exit()
 
-while True:
+#define the codec and create a VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+"""
+access some of caps features with cap.get(propId)
+propId -> 0-18 and denotes a property from the video
+
+in contrast some of these properties can be modified using .set(propId, value)
+"""
+
+
+while cap.isOpened():
     #capture frame by frame
+    #cap.read() returns true if frame is read correctly
     ret, frame = cap.read()
 
     #if frame is read correctly ret is True
     if not ret:
         print("cant receive frame(end stream?) exiting...")
         break
-    #our operations on the fram come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    
+    
+    frame = cv.flip(frame, 0)
+    #write the flipped frame
+    out.write(frame)
+
+
     #display the resulting frame
-    cv.imshow('frame', gray)
+    cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
         break
 
-#when everything is done, release the capture
+#when everything is done, release everything
 cap.release()
+out.release()
 cv.destroyAllWindows()
