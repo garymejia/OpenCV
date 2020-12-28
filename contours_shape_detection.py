ab16d701740ec52da1726db2768ea3236eb70576
garymejia <fjgmejia@gmail.com>
2020-12-28 01:26:52 -0500

    shape detection added for circle rectangle and square

--- contours_shape_detection.py
+++ contours_shape_detection.py
@@ -17,7 +17,14 @@ def getContours(img):
 
             if objCor == 3:
                 objectType = "Tri"
-            else if objCor == 4:
+            elif objCor == 4:
+                aspRatio = w/float(h)
+                if aspRatio > 0.95 and aspRatio < 1.05:
+                    objectType = "Square"
+                else:
+                    objectType = "Rectangle"
+            elif objCor > 4:
+                objectType = "Circle"
             else:
                 objectType = "None"
             cv.rectangle(imgContour, (x, y), (x+w, y+h), (0, 255, 0), 2)
