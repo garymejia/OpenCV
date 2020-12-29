df6951d996a89a9d303b62bcfe9ca1f1e83b3d72
garymejia <fjgmejia@gmail.com>
2020-12-29 11:38:31 -0500

    use haarcascade frontal face detection to detect a face using lena.png

---
+++ face_detection.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+faceCascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+img = cv.imread("lena.png")
+imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+
+for(x, y, w, h) in faces:
+    cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+
+cv.imshow("Result", img)
+cv.waitKey(0)
